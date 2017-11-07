f0e967d52a76132287039b875d9738da7adad3b3
Alexey Mogilyovkin <zeroicq@gmail.com>
2017-11-07 22:15:49 +1000

    Update db structure

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
+import fdb
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    con = fdb.connect(
+        dsn='db/TIMETABLE.FDB',
+        user='SYSDBA',
+        password='masterkey',
+        charset='UTF8'
+    )
+
+    cur = con.cursor()
+    query = 'SELECT * FROM WEEKDAYS';
+    cur.execute(query);
+    s = ''
+
+    for r in cur.fetchall():
+        s += str(r) + '<br>'
+    return s
