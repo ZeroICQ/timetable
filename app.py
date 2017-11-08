39374816d986e2c74f6d35ef9ef121e078abc65b
Alexey Mogilyovkin <zeroicq@gmail.com>
2017-11-08 15:26:54 +1000

    Work update

--- app.py
+++ app.py
@@ -2,24 +2,50 @@ import fdb
 from flask import Flask
 from flask import request
 from flask import render_template
-import pprint
+from flask import g
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    con = fdb.connect(
-        dsn='db/TIMETABLE.FDB',
-        user='SYSDBA',
-        password='masterkey',
-        charset='UTF8'
-    )
 
-    cur = con.cursor()
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'fb_db'):
+        g.fb_db = fdb.connect(
+            dsn='db/TIMETABLE.FDB',
+            user='SYSDBA',
+            password='masterkey',
+            charset='UTF8'
+        )
+    return g.fb_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'fb_db'):
+        g.fb_db.close()
+
+
+def get_tables():
+    cur = get_db().cursor()
+    query = '''select 
+                   rdb$relation_name 
+               from 
+                   rdb$relations 
+               where rdb$view_blr is null 
+                   and (rdb$system_flag is null or rdb$system_flag = 0)'''
+    cur.execute(query);
+    tables = tuple([x[0] for x in cur.fetchall()])
+    return tables
+
+
+@app.route("/")
+def index():
+    cur = get_db().cursor()
     query = 'SELECT * FROM WEEKDAYS';
     cur.execute(query);
-    s = ''
+    return render_template('list.html', tables=get_tables(), entries=cur.fetchall())
 
-    for r in cur.fetchall():
-        s += str(r) + '<br>'
-    return render_template('list.html', entries=map(lambda x: str(x), cur.fetchall()))
