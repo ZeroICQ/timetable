4264237ac244ab30e4c9bba32a8998fff10d1158
Alexey Mogilyovkin <zeroicq@gmail.com>
2017-11-08 23:06:32 +1000

    Add schedule controller

--- app.py
+++ app.py
@@ -73,3 +73,33 @@ def index():
         data['entries'] = cur.fetchall()
 
     return render_template('list.html', **data)
+
+@app.route("/schedule")
+def schedule():
+    con = get_db()
+    cur = con.cursor()
+    data = {}
+    data['tables'] = get_tables()
+
+    cur.execute('''
+        SELECT
+            sc_it.id,
+            ls.name,
+            sub.name,
+            aud.name,
+            grp.name,
+            tch.name,
+            tps.name,
+            wkd.name
+        from SCHED_ITEMS sc_it
+        LEFT JOIN LESSONS ls on ls.id = sc_it.lesson_id
+        LEFT JOIN SUBJECTS sub on sub.id = sc_it.subject_id
+        LEFT JOIN AUDIENCES aud on aud.id = sc_it.audience_id
+        LEFT JOIN GROUPS grp on grp.id = sc_it.group_id
+        LEFT JOIN TEACHERS tch on tch.id = sc_it.teacher_id
+        LEFT JOIN LESSON_TYPES tps on tps.id = sc_it.type_id
+        LEFT JOIN WEEKDAYS wkd on wkd.id = sc_it.weekday_id
+        ''')
+    data['entries'] = cur.fetchall()
+    data['selected_table'] = 4
+    return render_template('list.html', **data)
